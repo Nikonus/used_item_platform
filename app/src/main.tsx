4b73e756795082a4e157769c10143541fcf25a35
Nikonus <nikhildubay.2005@gmail.com>
2026-01-29 20:14:12 +0530

    feat: update global styles and app entry configuration

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
 import { AuthPage } from './pages/AuthPage'
+import { AddItemPage } from './pages/AddItemPage'
 import { DashboardPage } from './pages/DashboardPage'
+import { EditItemPage } from './pages/EditItemPage'
+import { HomeFeedPage } from './pages/HomeFeedPage'
+import { ItemDetailPage } from './pages/ItemDetailPage'
+import { MyListingsPage } from './pages/MyListingsPage'
 import { ProfilePage } from './pages/ProfilePage'
 import { PrivateRoute } from './routes/PrivateRoute'
 
@@ -15,9 +20,13 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/auth" element={<AuthPage />} />
           <Route element={<PrivateRoute />}>
-            <Route path="/" element={<DashboardPage />} />
+            <Route path="/" element={<HomeFeedPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/items/new" element={<AddItemPage />} />
+            <Route path="/items/:id" element={<ItemDetailPage />} />
+            <Route path="/items/:id/edit" element={<EditItemPage />} />
+            <Route path="/my-listings" element={<MyListingsPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
