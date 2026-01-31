728e6df345f025b46712623f646894e080742d58
Nikonus <nikhildubay.2005@gmail.com>
2026-01-31 17:09:01 +0530

    feat: add private route for protected pages

--- app/src/routes/PrivateRoute.tsx
+++ app/src/routes/PrivateRoute.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Outlet } from 'react-router-dom'
+import { LoadingSkeletonCard } from '../components/LoadingSkeleton'
 import { useAuth } from '../context/AuthContext'
 
 export const PrivateRoute = () => {
@@ -7,7 +8,7 @@ export const PrivateRoute = () => {
   if (loading) {
     return (
       <div className="fullscreen-center">
-        <div className="card">Loading...</div>
+        <LoadingSkeletonCard />
       </div>
     )
   }
